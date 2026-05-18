010c2bfd12be23ddb244668b2bb884b69cfe592d
meltnoleg <endhyettonugraha29@gmail.com>
2026-05-18 11:55:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("Perhitungan IKA dan IKU")
-st.write(
-    "IKA merupakan/............. IKU merupakan........mhbkdmhkbfjoi........."
-)
+st.title("Kalkulator Sederhana")
+
+angka1 = st.number_input("Masukkan angka pertama")
+angka2 = st.number_input("Masukkan angka kedua
+
+operasi = st.selectbox("Pilih operasi",["Penjumlahan","Pengurangan","Perkalian","Pembagian"])
+if st.button("Hitung"):
+    if operasi == "Penjumlahan":
+        hasil = angka1 + angka2
+    elif operasi == "Pengurangan":
+        hasil = angka1 - angka2
+    elif operasi == "Perkalian":
+        hasil = angka1 * angka2
+    elif operasi == "Pembagian":
+        if angka2 != 0:
+            hasil = angka1 / angka2
+        else:
+            hasil = "Tidak bisa dibagi 0"
+st.success(f"Hasil: {hasil}")
