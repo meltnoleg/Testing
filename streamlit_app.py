ecf52f45f600740d9bf1e9e375c229da8408e6c8
meltnoleg <endhyettonugraha29@gmail.com>
2026-05-18 11:19:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Perhitungan IKA dan IKU")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "IKA merupakan/............. IKU merupakan................."
 )
